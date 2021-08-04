3dbdd174a2032dc3413f2ceac708d7f33e4fc81b
saylaww <saylaww@gmail.com>
2021-08-04 15:03:11 +0500

    CORS 222 commit

--- src/main/java/com/example/herotestuser/config/WebMvc.java
+++ src/main/java/com/example/herotestuser/config/WebMvc.java
@@ -1,9 +1,11 @@
 package com.example.herotestuser.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvc {
 
     @Bean
